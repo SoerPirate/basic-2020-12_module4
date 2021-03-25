3c5f825c0b83bfa2d72299526a020488c4bfd1e3
Ilya <50113149+Sukhov63@users.noreply.github.com>
2021-03-25 08:33:01 +0400

    after lesson 22

---
+++ Assets/Scripts/Ragdoll. iK/PlayerHealth.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class PlayerHealth : MonoBehaviour, IPunObservable
+{
+    public const int Max = 100;
+
+    [Range(0, Max)]
+    public int health;
+
+    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        if (stream.IsReading)
+            health = (int)stream.ReceiveNext();
+        else
+            stream.SendNext(health);
+    }
+
+    [PunRPC]
+    void DamageRPC(int damage)
+    {
+        health -= damage;
+        if (health < 0)
+            health = 0;
+    }
+
+    [PunRPC]
+    void GiveHealthRPC(int amount)
+    {
+        health += amount;
+        if (health > Max)
+            health = Max;
+    }
+}
